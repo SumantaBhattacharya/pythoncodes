9cfec054e46c9964f63ad101fef51e2f4146f0c9
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-02 11:08:21 +0530

    Add function definition and usage example in 11_function.py

---
+++ 11_function.py
@@ -0,0 +1,9 @@
+#fuctions are organised resuable code that is used to perform single related action 
+# Any variable that is created inside a function is called local variable and it is local to it
+def greet():#functin declaration
+    #function body
+    print("hello! Brother")
+    
+
+greet()#function calling
+greet()#reusability
