892c920ff106e7fa920700f723e266d961e8dd8f
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-02 11:08:05 +0530

    Add initial implementation of operator programming language examples in Python

---
+++ o.py
@@ -0,0 +1,35 @@
+'''An operatorprogramming language is a symbol that tells the compiler interpreter to perform specific mathematical relational or logical operation and produce final result for example plus minus division multiplication etc
+There are total 7 types of operators arithmetic operator comparison operator or relational operator assignment operator logical operator bitwise operator membership operator identity operator'''
+a=10
+b=5
+c="I love Python programming"
+a+=8#Assignment operator used to perform arithmetic Operations while assigning to a variable a=a+8
+print(a+b)
+print(a-b)
+print(a/b)
+print(a//b)
+print(a*b)
+print(a**b)#10^5 exponentitation power
+print(a%b)
+print(a+70)
+print(3+9)
+print(a)
+print(a>b)#Comparison operator used to compare the value of 2 operators and return boolean values true or false
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a==b)
+print(a!=b)
+print(a>b and a<b)#In Python there aretotal 3 types of logical operators the operator always return boolean value The                        # operator the output will be one true when both the inputs are one true and if there is zero in one or more than one input the output will always be zero
+print(a<b and a>b)
+print(a>b or a<b)
+print ( a<b or a>b)
+print(not(a>b and a<b))
+print(not(a>b))
+print(not( a<b))
+print(not(a>b or a<b))
+print(a is c)#Identity or  operator used to compare to values to determine if they point to the same object there are 2 types of identify operator in Pythonis and is not The ease operator returns to if both the operands point to the same object the Is not operator returns true if both operand do not point to the same object
+print(a is not b)
+print("love" in c)
+print("love" not in c)
+print(divmod(5,2))#function to find out quotient and remainder, assign values instead of assigning variable when using this function, it works with only integer values (unsupported operand type(s) for divmod(): 'str' )
