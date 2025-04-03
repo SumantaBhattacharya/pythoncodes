804146546bcee03d75c510eb4d97e4c9885d2c01
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-04 00:10:55 +0530

    Add connestedif.py to demonstrate nested conditional statements in Python

---
+++ connestedif.py
@@ -0,0 +1,18 @@
+
+x=1_000
+y=20
+if(x>y):
+    print("x>y")
+
+    if(x>70):
+        print("x>70")
+   
+    else:
+        print("x<20")
+   
+   
+    
+elif (x<y):
+    print("x<y")
+else:
+    print("x=y")
