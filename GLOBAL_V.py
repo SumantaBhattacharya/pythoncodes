57aa671206dd5185b53cfc7b80eadf4cf6cdde0b
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-04 00:10:39 +0530

    Add GLOBAL_V.py to demonstrate the use of global variables in Python

---
+++ GLOBAL_V.py
@@ -0,0 +1,16 @@
+# any variable that are created outside a function are called global variable and you can use this global variable From both inside and outside of a function
+message="How You Doing"#global_variable
+def greet():
+ 
+   
+    print(message)
+    
+
+   
+    
+greet()
+print(message)
+
+    
+
+
