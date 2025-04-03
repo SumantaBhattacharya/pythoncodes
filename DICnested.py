a9164e42345e1ef734716e4da3596d4cfc2623f9
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-03 23:19:22 +0530

    Add DICnested.py with a nested dictionary example and print statements

---
+++ DICnested.py
@@ -0,0 +1,11 @@
+CCI= {
+    "Father":{"name":"SUDIP BHATTACHARYA","SALARY":47000, "AGE":52 ,"MOOD":"T_T"},   
+"son":{"name":"Sumanta BHATTACHARYA","SALARY":12000,"AGE":18 ,"MOOD":" ☆*: .｡. o(≧▽≦)o .｡.:*☆ "},
+"currentadress":{"splace":"ASSAM","Splace":"KOLKATA"}# '=' = ':'
+}
+print("\n",CCI)
+print("\n",CCI["Father"])
+print("\n",CCI["Father"]["MOOD"])
+print(CCI["currentadress"]["splace"])
+
+
