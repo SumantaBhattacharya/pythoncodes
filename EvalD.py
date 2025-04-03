7edd6b876a6e6b4f94f92d807ef5b2339378fa75
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-04 00:10:05 +0530

    Add EvalD.py to demonstrate dynamic data evaluation and formatted printing in Python

---
+++ EvalD.py
@@ -0,0 +1,5 @@
+var=(eval(input("enter DATA:")))#this eval function reduces the problem of changing datatype
+print(var)
+print("hi","Sumata","Bhattacharya",sep="_")
+print("hello",end="\n\n")
+print("world")
