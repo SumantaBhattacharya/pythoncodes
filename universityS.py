ae9928d7c9fdcae0c7025dd2f925fdd9435b2ca8
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-11 13:49:53 +0530

    Add universityS.py to calculate tuition fee after applying an early payment discount

---
+++ universityS.py
@@ -0,0 +1,7 @@
+# suppose you are a university student and you need to pay $4535 tution fee for the next semester.
+# The collage is giving you a discount of 10 % on the early payment of your tution fees since its a good offer you decided to make an early payment. 
+#can you find out how much money you have to pay 
+fee = 4535
+Discount_percent = 10
+discount_amount = (fee/Discount_percent  )* 100
+print(discount_amount)
