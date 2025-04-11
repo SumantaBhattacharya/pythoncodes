a475c8a603f2321a0b1b6337808b2817f48bf097
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-11 13:50:04 +0530

    Add loopW.py to demonstrate a while loop that prints "Hello world" five times

---
+++ loopW.py
@@ -0,0 +1,17 @@
+'''Loop is a process of executing a block of statements repeatedly until certain condition is met
+While loop checks its condition first then execute loop body
+While loop has no ; In the end
+Syntax while(condition):
+{
+iteration
+}
+initialization condition And iteration are done in different lines of the code
+while loop runs continiously until the test condition is false. if the test condition of the lopp is never false , the loop run infinitely until the memory runs out '''
+i=1 #initialization
+while i<=5:#condition
+
+
+    print("Hello world", end=" ")# Using end=" " to print in a single line with a space as separator
+    i+=1#i++ iteration(increment and decrement)
+
+
