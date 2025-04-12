8acf9080cfe0b412063ce3c8ea269011d427bcd5
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-13 00:25:38 +0530

    Add math2.py to demonstrate importing specific functions from the math module

---
+++ math2.py
@@ -0,0 +1,4 @@
+#When we only need to import a specific definition like a function or a constant we can use the firm import statement
+from math import pi, sin, sqrt#or *
+value = sin (pi/2)
+print(value)
