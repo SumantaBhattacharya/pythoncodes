f6385ebf509354bafd0414e027235267e02548aa
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-13 00:25:51 +0530

    Add oustingfunction.py to implement basic arithmetic operations with user input

---
+++ ousingfunction.py
@@ -0,0 +1,16 @@
+def operations(n1,n2):
+    print(n1+n2)
+    print(n1-n2)
+    print(n1*n2)
+    print(n1/n2)
+    print(n1%n2)
+    print(n1//n2)
+    print(n1**n2)#exponent
+
+
+
+n1=input("enter an integer") 
+n2=input("enter an integer value ")
+operations (int(n1),int(n2 ))
+
+
