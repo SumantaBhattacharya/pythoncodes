cc9dfba1b0cb64269c40fdef6e1abaf1ea395133
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-12 10:32:50 +0530

    Add continue.py to demonstrate the use of the continue statement in a loop

---
+++ continue.py
@@ -0,0 +1,8 @@
+
+
+animal=["Tiger","cat","dog"]
+for i in animal:
+   # print(i,end=" ")
+    if i=="cat":#Continue statement. Continue statement terminates the execution of that current itearation of the loop
+        continue #Continue the loop after cat.
+    print(i)
