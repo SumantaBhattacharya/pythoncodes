cbd72672c7e70e7b7fd5e88115740c0fdcf9daa2
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-13 00:25:02 +0530

    Add loopNested_YCH.py to demonstrate nested for loops with animal sounds

---
+++ loopNested_YCH.py
@@ -0,0 +1,8 @@
+#nested for loop
+animal=["tiger","cat","dog"]
+sound=["roar","meow","barks"]
+for x in animal:
+    # body of x for loop
+    for y in sound:
+        #body of nested while y for loop
+        print("The "+ x +" ",y)
