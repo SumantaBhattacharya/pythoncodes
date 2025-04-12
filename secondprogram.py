9c9d9701b27000d41ad300d4dae1ebc23277df34
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-12 10:30:38 +0530

    Add secondprogram.py to demonstrate various programming concepts and data types in Python

---
+++ secondprogram.py
@@ -0,0 +1,81 @@
+#Variable is the name of memory location where we can store different types of values such as username biodata Etc| a variable name should start with a uppercase or lowercase alphabet|a variable name cant start with a number or with a symbol except underscore(_) | no widespace and symbol is allowed in between the variable | keyword names cannnot be used as variable  
+'''There are 2 types of Programming languages high level language and low level language 
+low level programming language Types one machine language 2 assembly language
+Machine language is a lower level programming language which is compiled of binary digits 1to0 machine language is not human readable
+Machine language execution time is very fast
+Assembly language is a low level programming language which is human readable and execution time is slow
+Assembly language converts into machine language by the assembler and then execute
+We use numbers symbols to write assembly languageMachine language and assembly language are both machine and hardware dependent
+High level programming language examples C plus plus C programming Java JavaScript Python
+hll language is a human readable high level language is execution time is slow
+High level language is machine and hardware independent
+ high level language converts into machine languageby the compiler or interpreter and then execute
+Keywords are reserved words whose meaning is already defined by the compiler example integer float char
+Compiler is a software program that converts high level source code into low level object code
+Compiler scans the entire programme at once
+comPiler based programming languages are C&C plus plus
+Interpreter is a software program that converts high level source code into intermediate code intermediate code is used to translate source code into machine code and then convert it into machine code 
+interpreter scans line by line statement of a program
+Interpreted the execution time is slow and interpreter based programming languages are JavaScript Python
+Comments are used to write something which The programmer does not want to execute completely ignored by the compiler comments are non executable by the compiler
+datatype type is a classification that specifies which type of value in variable has and what type of operation can be applied to it without causing an error
+the data that are declared within class are called data members 
+the function that are declared within the class are called member function
+linear data structure is a data structure representing linear relationship
+data abstraction is a process of representing essential features without including the background details
+,Database is a collection of Inter relation data items stored together to serve multiple operations
+ database functions are database ensure security of data 2 database facilitates sharing of data 
+ data types are primitive and non primitive types they can store only one type of data primitive data type examples are floatingpoint integer char aboolean 
+non primitive data type is a collection where we can store more than one type of data non primitive data type examples are array string class
+Numeric data types in Python numeric data types represent the data type which has numerical values numerical value can be integer floating or even complex numbers zero is neither positive nor negative integer
+All positive numbers are included in set of natural numbers
+ all positive numbers and zero are included in set of whole numbers 
+natural numbers are start from one 
+whole numbers are start from zero
+Note a value which has a decimal point is called floating point. It is a real number
+Integer means a whole number. This value is represented in Python int it contains positive and negative whole numbers
+A string is basically a sequence of one or more charactersenclosed in a single quotation or double quotation and triple quotation
+In double quotes string we can include single quote but we cannot include double quote we can include single quote in double code string in the middle
+in triple quote string we can include multiple lines string which is not possible in a single or double quotes string even we can include single or double quote in it
+The value of boolean are always true or false 
+boolean data type is one of the build in data type
+ Python is a general purpose dynamically typed high level language developed by Guida Van Rossum in the year 1991at CWI Centrum Visconda and information in Nether land
+ To check that the program name called python is installed in your compuuter system or not we need to type py in your cmd (command promt)
+It is a interpreter language
+IDLE(Integerated Devlopment learning Environment) is a platform to code in python. file >>  new file >> run >> file name >> save
+control + s = save 
+Guido Van Rossun was a fan of popular comedy show "monty pythons flying circus" broad casted by BBC (1969-74),so he decided to pick named hes language as python
+features
+1)simple and easy to loan 
+2) freeware and open source (can make changes) 
+3) Platform independent
+4) Rick Library
+5) Portable 
+6)Embedded(can be used in other languages)
+7) Extensible (other programming languages can be used in python)
+8)Interpreted (read each line at a time )
+Applications
+Web devlopment 2) Game devlopment 3) IOT programming 4) Machine learning 5) Artificial Intelegence 6) Data Base programming 7) Network programming
+Limitaions 1) Mobile Applications (we cannot create apps using python) 2) performance is not up to the network
+Q)How is a clode block indiacated in python ~openweaver
+ans>Identation
+'''
+age=-18
+fruits="oranges"
+marks=69.8
+fruits="apple"
+b=True
+print(age)
+print(fruits)
+print(marks)
+print(type(age))
+print(type(fruits))
+print(type(marks))
+print(id(age))
+print(id(fruits))
+print(b)
+print(type(b))
+print(len(fruits))#value length
+print(pow(age,2))#power
+print(abs(age))#absolute value
+print(divmod(5,2))#function to find out quotient and remainder, assign values instead of assigning variable when using this function
