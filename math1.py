1f2b3179080578d36f4049a3ce64cdd926cc58f2
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-12 10:34:02 +0530

    Add math1.py to demonstrate various mathematical functions and operations using the math module

---
+++ math1.py
@@ -0,0 +1,34 @@
+import math #This module contains many mathematical functions and constraints.We are using square root function from this module.This function is used to calculate square root of a number
+num = 25 #5*5
+print(num)
+
+num1=math.sqrt(num)
+print(num1)
+
+print(math.pi)
+
+print(type(math))
+print(id(math))#object of type 'module' has no len()
+print(dir(math))
+
+print(math.pow(num,3))
+
+num2=math.isqrt(num)
+print(num2)
+
+import math as m #While importing a module we can also rename it to a different name as per our needs
+
+num3=m.cbrt(27)#3*3*3=27
+print(num3)
+
+result = math.ceil(6.9)#convert float to integer by ych
+print(result)
+
+pi=math.pi#by ych
+print(pi)
+
+print(math.cos(num))#by ych
+print(math.sin(num))
+print(math.tan(num))
+print(math.cos(num))
+print(math.cosh(num))#coshic theta
