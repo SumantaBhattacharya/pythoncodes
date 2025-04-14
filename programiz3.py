088cc6000f451d5f52bf3979166e2d313c9627b4
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-14 11:27:47 +0530

    Add programiz3.py to demonstrate conditional statements and output evaluation

---
+++ programiz3.py
@@ -0,0 +1,10 @@
+# what will be the output of the following code
+x=2
+y=5
+if(not x % 2) and  ( y % 2):
+    print(1)
+elif (x==2):
+    print(2)
+else:
+    print(3)
+# x%2 i.e 2%2 gives remainder 0 and no 0 equals to 1 which means "True" and y %2 i.e 5%2 gives remainder 1 which means " True" Both are true so it print 1 
