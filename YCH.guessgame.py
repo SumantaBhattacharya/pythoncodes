93e0474b0e425b8db992cb22bf917e92234a5ac7
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-14 11:23:34 +0530

    Add YCH.guessgame.py to implement a number guessing game in Python

---
+++ YCH.guessgame.py
@@ -0,0 +1,16 @@
+my_num=3
+while(True):
+ print("I have a number in my mind. Can you guess it?. It is in between one to 10?")
+ guess=(int(input("Enter Your Number:")))#If we Leave the int part, then it will take string as it data type
+
+
+ if (guess==my_num):
+    print("Yes, you are right.")
+    break
+ elif(guess>my_num):
+    print("My number is greater than the number you have entered \n try again.")
+ elif(guess<my_num):
+    print("My number is lower than the number you have entered.")
+ else:
+    print("Invalid")
+
