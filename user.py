0b0c52a76b09d6004078cdbc3fb37fb14bbeea80
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-22 00:23:34 +0530

    Add user.py to demonstrate input and output functions in Python

---
+++ user.py
@@ -0,0 +1,19 @@
+#input/output
+'''in python
+for input we have input()
+for output we have print()
+input()function 
+> In python programs, we use the input () function to take any kind of data input from the user through the variable 
+> The input() function waits for the  user input and as soon as the user enters the data and presses the enter key, the input () fucntion reads that data and assigns to the variable
+syntax
+variable_name= input("")
+>input()function always returns string value (character/alphabets)
+print function
+> it is used to desplay the output on the console
+print()function. default datatype is string
+
+ '''
+
+num1=input("enter an integer") 
+num2=input("enter an integer value ")
+print(num1+num2)
