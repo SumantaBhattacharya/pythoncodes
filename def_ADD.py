768124fc43568ceda8ad7a30726c141bcb2ada78
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-27 08:47:15 +0530

    Add def_ADD.py to implement a function for adding two numbers with a default parameter

---
+++ def_ADD.py
@@ -0,0 +1,13 @@
+
+#here n1 is the default value
+def add(n1=6.9 ,n2= 8):
+    sum = n1 + n2
+    
+    return (sum)
+
+result = add(2)# the first number is a default of n1 and if you give the second number then n2 will be default number
+print( "",result)
+
+r = add(9.8)
+print( "",r)
+print(type(add))
