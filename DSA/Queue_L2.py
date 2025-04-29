5a9be496c8d629c6045bb607756d30579cafb072
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-29 23:55:23 +0530

    Add Queue_L2.py to implement a queue with basic operations and example usage

---
+++ DSA/Queue_L2.py
@@ -0,0 +1,15 @@
+# [69, 77, 99, 104, 420]--->
+queue = []
+queue.insert(0,69)
+queue.insert(0,6.9)
+queue.insert(0,6)
+queue.insert(0,9)
+print(queue)
+print(queue.pop())
+print(queue.pop())
+print(queue.pop())
+print(queue.pop())
+print(queue)
+print(not queue)
+#print((queue[-1]))# Last entered element
+#print((queue[0]))#  first entered element does not work
