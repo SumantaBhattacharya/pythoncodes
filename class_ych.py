61024b26a9194a8e31d2e868d3b5bbad8a48ad9d
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-30 00:06:47 +0530

    Add class_ych.py to define a person class and demonstrate object creation and property access

---
+++ class_ych.py
@@ -0,0 +1,14 @@
+class person:
+    first_name= "Indranil"
+    last_name = "Chakraborty"
+    age=20
+
+#creating a object
+#syntax class_name()
+person_obj=person()#class object
+#access properties
+name=person_obj.first_name
+print("first_name=",name)
+years=person_obj.age
+print(years)
+
