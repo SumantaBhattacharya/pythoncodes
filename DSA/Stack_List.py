90b5f1ba8b0fc4d83dc39639c3b9989d633ee2f8
SumantaBhattacharya <sumanta2004@gmail.com>
2025-04-30 00:09:04 +0530

    Add Stack_List.py to implement stack operations using a list in Python

---
+++ DSA/Stack_List.py
@@ -0,0 +1,20 @@
+# Is a linear data Structure and ordered collection of items Where the edition of new items and removable of items?always take place at the same end.last in first out, the removal of items willstart from the top.
+#There are several operation that we can perform in this stack.push operation, which add elements to the stack pop operation, which removes the elements from the peak or top operation.Give you the element present on the top
+# Is empty operation to check the stack is empty or not. Is full operation to check the stack is full or not.
+#In python stack is not a build in data structure. We.can implement stack using list or modules.
+Stack = []
+Stack.append(68) # Any object can be accessed only from the top.  
+Stack.append(69) # Any object can be added only at the top. 
+Stack.append(77) # When we define stack as an adt, abstract data type, then we only interested in knowing the stack operations from the user point of view.
+print(len(Stack))
+print(len == 0) #To check stack.is empty or not?
+print(Stack[0])
+print(Stack[-1])#
+print(Stack)
+Stack.pop()
+print("The first element to be poped out:",Stack)
+Stack.pop()
+Stack.pop()
+print(Stack)
+print(not(Stack))#To check stack.is empty or not?
+
